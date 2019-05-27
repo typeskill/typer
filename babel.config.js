679fc7cb1794f127a37784df173c45cb9b203fdc
Jules Randolph <jules.sam.randolph@gmail.com>
2019-05-27 17:10:23 +0200

    build: switch back to metro babel preset

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     presets: [
-      "@babel/preset-env",
-      "@babel/preset-typescript"
+      "module:metro-react-native-babel-preset"
     ],
     plugins: [
       ["@babel/plugin-proposal-decorators", { legacy: true }],
