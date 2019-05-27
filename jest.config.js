9be2142a42a350374fda1be8a65712f56c6aabcf
Jules Randolph <jules.sam.randolph@gmail.com>
2019-05-27 14:42:12 +0200

    build(test): disable jest coverage reports

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,6 @@ module.exports = {
     // This is the only part which you can keep
     // from the above linked tutorial's config:
     cacheDirectory: '.jest/cache',
-    "collectCoverage": true,
-    "coverageReporters": ["json"],
+    "collectCoverage": false
 };
   
