946d76998380d8b5380e80b94c4ff09b035daaa0
Jules Randolph <jules.sam.randolph@gmail.com>
2019-05-28 10:14:02 +0200

    feat: expose classes and types to module root

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 import Sheet from '@components/Sheet'
-import Toolbar from '@components/Toolbar'
+import Bridge from '@core/Bridge'
+import {
+  TextAttributesMap,
+  BlockAttributesMap
+} from '@delta/attributes'
+import {
+  BaseTextTransformAttribute,
+  TextLineType,
+  TextLengthModifierLineType,
+  BooleanTextTransformSpec,
+  TextTransformSpec,
+  TextTransformsDictionnary
+} from '@delta/transforms'
+import DocumentDelta, {
+  DeltaChangeContext,
+  DocumentLine,
+  GenericDelta
+} from '@delta/DocumentDelta'
 
 export {
-  Sheet as RichEditor,
-  Toolbar
+  Sheet,
+  Bridge,
+  // @delta/attributes
+  TextAttributesMap,
+  BlockAttributesMap,
+  // @delta/transforms
+  BaseTextTransformAttribute,
+  TextLineType,
+  TextLengthModifierLineType,
+  BooleanTextTransformSpec,
+  TextTransformSpec,
+  TextTransformsDictionnary,
+  // @delta/DocumentDelta
+  DocumentDelta,
+  DeltaChangeContext,
+  DocumentLine,
+  GenericDelta
 }
