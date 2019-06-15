fe228be0ff6c5570ac022f22527bf1f2aae9a5b2
Jules Randolph <jules.sam.randolph@gmail.com>
2019-06-16 00:52:46 +0200

    chore(ex): adapt to new API

--- examples/expo/App.tsx
+++ examples/expo/App.tsx
@@ -5,9 +5,8 @@ import {
   Bridge,
   Sheet,
   Toolbar,
-  TextControlAction,
-  ToolbarLayout,
-  TEXT_CONTROL_SEPARATOR,
+  ControlAction,
+  CONTROL_SEPARATOR,
   buildVectorIconControlSpec,
 } from 'react-native-typeskill'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
@@ -18,47 +17,45 @@ if (typeof (global as any).self === 'undefined') {
   ;(global as any).self = global
 }
 
-function buildMaterialControlSpec(actionType: TextControlAction, name: string) {
+function buildMaterialControlSpec(actionType: ControlAction, name: string) {
   return buildVectorIconControlSpec(MaterialCommunityIcons as any, actionType, name)
 }
 
-const toolbarLayout: ToolbarLayout = [
-  buildMaterialControlSpec(TextControlAction.SELECT_TEXT_BOLD, 'format-bold'),
-  buildMaterialControlSpec(TextControlAction.SELECT_TEXT_ITALIC, 'format-italic'),
-  buildMaterialControlSpec(TextControlAction.SELECT_TEXT_UNDERLINE, 'format-underline'),
-  buildMaterialControlSpec(TextControlAction.SELECT_TEXT_STRIKETHROUGH, 'format-strikethrough-variant'),
-  TEXT_CONTROL_SEPARATOR,
-  buildMaterialControlSpec(TextControlAction.SELECT_LINES_ORDERED_LIST, 'format-list-numbered'),
-  buildMaterialControlSpec(TextControlAction.SELECT_LINES_UNORDERED_LIST, 'format-list-bulleted'),
+const toolbarLayout: Toolbar.Layout = [
+  buildMaterialControlSpec(ControlAction.SELECT_TEXT_BOLD, 'format-bold'),
+  buildMaterialControlSpec(ControlAction.SELECT_TEXT_ITALIC, 'format-italic'),
+  buildMaterialControlSpec(ControlAction.SELECT_TEXT_UNDERLINE, 'format-underline'),
+  buildMaterialControlSpec(ControlAction.SELECT_TEXT_STRIKETHROUGH, 'format-strikethrough-variant'),
+  CONTROL_SEPARATOR,
+  buildMaterialControlSpec(ControlAction.SELECT_LINES_ORDERED_LIST, 'format-list-numbered'),
+  buildMaterialControlSpec(ControlAction.SELECT_LINES_UNORDERED_LIST, 'format-list-bulleted'),
 ]
 
 const themeColor = '#ffffff'
 const iconSize = 25
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   private bridge: Bridge
-  constructor(props: {}) {
+  public constructor(props: {}) {
     super(props)
     this.bridge = new Bridge()
   }
 
-  render() {
-    const sheetEventDom = this.bridge.getSheetEventDomain()
-    const controlEventDom = this.bridge.getControlEventDomain()
+  public render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <View style={styles.container}>
           <KeyboardAvoidingView style={{ flex: 1 }} enabled>
             <Version />
             <Card style={{ flex: 1, marginHorizontal: 10, marginTop: 4 }}>
-              <Sheet contentContainerStyle={{ marginBottom: iconSize + 4 }} sheetEventDom={sheetEventDom} />
+              <Sheet contentContainerStyle={{ marginBottom: iconSize + 4 }} bridge={this.bridge} />
             </Card>
             <View style={styles.toolbarContainer}>
               <Toolbar
                 iconSize={iconSize}
                 layout={toolbarLayout}
                 contentContainerStyle={{ backgroundColor: '#eaf0fc' }}
-                bridgeOuterInferface={controlEventDom}
+                bridge={this.bridge}
               />
             </View>
           </KeyboardAvoidingView>
