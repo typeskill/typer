a98f3e82071206a299316515a31a5ac0aa096365
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2019-09-23 09:22:14 -0300

    feat: export TextOp, ImageOp and BlockOp

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ import {
 } from '@components/Toolbar'
 import { Attributes } from '@delta/attributes'
 import { GenericRichContent } from '@delta/generic'
-import { GenericOp, TextOp } from '@delta/operations'
+import { GenericOp, TextOp, ImageOp, BlockOp } from '@delta/operations'
 import { Transforms, defaultTextTransforms } from '@core/Transforms'
 import { Document, buildEmptyDocument, cloneDocument } from '@model/document'
 import { SelectionShape } from '@delta/Selection'
@@ -66,10 +66,12 @@ export {
   // Model related
   Bridge,
   Images,
+  SelectionShape,
   GenericRichContent,
   GenericOp,
-  SelectionShape,
   TextOp,
+  ImageOp,
+  BlockOp,
   Attributes,
   Transforms,
   Document,
